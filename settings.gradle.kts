53c530f92095cfdea7464126bd9d7b32ed10b936
righthandman176@proton.me <raikmaus321@gmail.com>
2024-12-15 22:16:20 +0100

    Update to stonecutter 0.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,11 @@ pluginManagement {
         maven("https://maven.architectury.dev")
         maven("https://maven.minecraftforge.net")
         maven("https://maven.neoforged.net/releases/")
-        maven("https://maven.kikugie.dev/snapshots")
     }
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.5-beta.3"
+    id("dev.kikugie.stonecutter") version "0.5"
 }
 
 stonecutter {
